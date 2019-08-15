b133e23fc4d63a3dd1067ec3a9e3e01b6c9a4a47
GOSSAN0602 <shukigotoshalab@gmail.com>
2019-08-15 00:11:56 +0000

    param4

--- libs/get_dictionaries.py
+++ libs/get_dictionaries.py
@@ -10,9 +10,9 @@ def get_estimators_dict():
 
 def get_params_dict():
     types=['1JHC', '2JHH', '1JHN', '2JHN', '2JHC', '3JHH', '3JHC', '3JHN']
-    p_1JHC = {'num_leaves': 200,
+    p_1JHC = {'num_leaves': 400,
               'objective': 'huber',
-              'max_depth': 7,
+              'max_depth': 8,
               'learning_rate': 0.12,
               "boosting_type": "gbdt",
               "subsample_freq": 1,
@@ -40,7 +40,7 @@ def get_params_dict():
  
     p_1JHN = {'num_leaves': 200,
               'objective': 'huber',
-              'max_depth': 6,
+              'max_depth': 4,
               'learning_rate': 0.12,
               "boosting_type": "gbdt",
               "subsample_freq": 1,
@@ -52,9 +52,9 @@ def get_params_dict():
               'colsample_bytree': 1.0,
              }
  
-    p_2JHN = {'num_leaves': 300,
+    p_2JHN = {'num_leaves': 200,
               'objective': 'huber',
-              'max_depth': 7,
+              'max_depth': 5,
               'learning_rate': 0.12,
               "boosting_type": "gbdt",
               "subsample_freq": 1,
@@ -66,9 +66,9 @@ def get_params_dict():
               'colsample_bytree': 1.0,
              }
 
-    p_2JHC = {'num_leaves': 400,
+    p_2JHC = {'num_leaves': 500,
               'objective': 'huber',
-              'max_depth': 8,
+              'max_depth': 9,
               'learning_rate': 0.12,
               "boosting_type": "gbdt",
               "subsample_freq": 1,
@@ -80,9 +80,9 @@ def get_params_dict():
               'colsample_bytree': 1.0,
              }
 
-    p_3JHH = {'num_leaves': 400,
+    p_3JHH = {'num_leaves': 500,
               'objective': 'huber',
-              'max_depth': 8,
+              'max_depth': 9,
               'learning_rate': 0.12,
               "boosting_type": "gbdt",
               "subsample_freq": 1,
@@ -93,9 +93,9 @@ def get_params_dict():
               'colsample_bytree': 1.0,
              }
 
-    p_3JHC = {'num_leaves': 450,
+    p_3JHC = {'num_leaves': 500,
               'objective': 'huber',
-              'max_depth': 8,
+              'max_depth': 9,
               'learning_rate': 0.12,
               "boosting_type": "gbdt",
               "subsample_freq": 1,
@@ -107,9 +107,9 @@ def get_params_dict():
               'colsample_bytree': 1.0,
              }
 
-    p_3JHN = {'num_leaves': 400,
+    p_3JHN = {'num_leaves': 300,
               'objective': 'huber',
-              'max_depth': 7,
+              'max_depth': 6,
               'learning_rate': 0.12,
               "boosting_type": "gbdt",
               "subsample_freq": 1,
